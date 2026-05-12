3999e47407fbc34b0092ca48548ee2c196685cdf
Thierry Vignaud <thierry.vignaud@gmail.com>
2026-05-12 13:22:11 +0200

    Prettier BMS description

--- gen-new-ids-bms-all2.pl
+++ gen-new-ids-bms-all2.pl
@@ -196,6 +196,7 @@ sub process {
 	$commune =~ s/<[^>]*>//g;
 	$commune =~ s/ \(Finist.*re\)//;
 	$type = ucfirst($type);
+	$type =~ s/Registre paroissial (registre )?baptême mariage sépulture administration administration générale structure administrative administration communale collectivité locale commune société population état civil( décès mariage naissance)?/BMS/;
 	# Use the IDS that appears in old permalinks, eg: 'AN08' as in 'FRAD029_3E010_0028_00D_AN08_007.jpg':
 	my $year = $desc;
 	my ($ark) = $link =~ m!/ark:/72506/([^/]+)/!;
